2be5c9f32a058f6b5dd78c58e4698169e497af32
Nikita Mitasov <me@ch4og.com>
2025-12-24 04:34:19 +0300

    chore(nix): init flake and direnv

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "CSM SSBU Mapban";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [git reuse docker nodejs];
+        shellHook = "npm ci";
+      };
+    });
+}
